ccb1c2a66c98d15c7c56e6348d748e4bd6b91062
tsamui <tsamui@users.noreply.github.com>
2015-08-18 14:56:23 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+  x<<-y
+  m<<-NULL
 }
+get<-function() x
+setmatrix<-function(solve) m<<- solve
+getmatrix<-function() m
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
+}      
 
 
 ## Write a short comment describing this function
