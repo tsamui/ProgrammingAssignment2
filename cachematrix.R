b7e94ebd293fe45e00c527151c3abbed363f472d
tsamui <tsamui@users.noreply.github.com>
2015-08-18 14:59:39 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -22,4 +22,14 @@ list(set=set, get=get,
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m<-x$getmatrix()
+    if(!is.null(m)){
+      message("getting cached data")
+      return(m)
+    }
+    matrix<-x$get()
+    m<-solve(matrix, ...)
+    x$setmatrix(m)
+    m
+}
 }
